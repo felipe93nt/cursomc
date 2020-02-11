3f0dbaaff8e10f6e8003356150ffb47cd703d38b
Felipe Augusto <felipe.93nt@gmail.com>
2020-02-10 21:34:21 -0300

    configurando conexao com mysql e configurando spring para gerar classe categoria em automatico

--- src/main/java/com/felipeaugusto/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/felipeaugusto/cursomc/resources/CategoriaResource.java
@@ -15,7 +15,7 @@ public class CategoriaResource {
 	@RequestMapping(method = RequestMethod.GET)
 	public List<Categoria> listar() {
 	
-	Categoria cat1 = new Categoria(1,"Informatica");
+	Categoria cat1 = new Categoria(1,"TI");
 	Categoria cat2 = new Categoria(2, "Escritório");
 	
 	List<Categoria> lista = new ArrayList<Categoria>();
